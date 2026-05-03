82a6701400eb7ce666f4a7b43442bbfb6a15d018
Khaled <Khaleid_atwi_97@live.com>
2026-05-03 19:47:12 +0300

    New Icons and some added arabic texts ,and moved the Preview and Print button to the right

--- Forms/BarcodeLabelsForm.cs
+++ Forms/BarcodeLabelsForm.cs
@@ -16,6 +16,7 @@ namespace GenericInventorySystem.Forms
         private DataGridView dgvItems;
         private ModernTextBox txtSearch;
         private ModernButton btnGenerate;
+        private FlowLayoutPanel pnlButtons;
         private Label lblTitle;
         private DataTable _dtItems;
         private System.Windows.Forms.Timer _searchTimer;
@@ -148,10 +149,32 @@ namespace GenericInventorySystem.Forms
             txtSearch.TextChanged += (s, e) => { _searchTimer.Stop(); _searchTimer.Start(); };
             pnlTop.Controls.Add(txtSearch);
 
+            pnlButtons = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                AutoSize = true,
+                Anchor = AnchorStyles.Top | AnchorStyles.Right,
+                Location = new Point(400, 50),
+                Height = 40,
+                WrapContents = false
+            };
+
             btnGenerate.Size = new Size(180, 40);
-            btnGenerate.Location = new Point(360, 50);
             btnGenerate.Click += btnGenerate_Click;
-            pnlTop.Controls.Add(btnGenerate);
+            pnlButtons.Controls.Add(btnGenerate);
+
+            pnlTop.Controls.Add(pnlButtons);
+
+            // RTL Awareness for positioning
+            pnlTop.Resize += (s, e) => {
+                if (LocalizationManager.IsArabic) {
+                    txtSearch.Location = new Point(pnlTop.Width - txtSearch.Width, 50);
+                    pnlButtons.Location = new Point(0, 50);
+                } else {
+                    txtSearch.Location = new Point(0, 50);
+                    pnlButtons.Location = new Point(pnlTop.Width - pnlButtons.Width, 50);
+                }
+            };
 
             tlpMain.Controls.Add(pnlTop, 0, 0);
 
